26d3cff3ae9e80c63c47290fc7593d1dca474097
BacaCesar <cesarbaca.fotografia@gmail.com>
2024-06-10 22:51:52 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -5,4 +5,19 @@ import '../scss/styles.scss';
 //- Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
 
 const numbers=[1,2,3,4,5]
-console.log(numbers)
+
+numbers.map((number,index)=>console.log(number*index))
+
+//console.log(numbers)
+
+//- Crea una función que reciba un array de números y devuelva un array con cada número dividido por su índice en el array más 2, es decir index + 2
+
+const arrayOfNumbers=[1,2,3,4,5]
+
+arrayOfNumbers.map((number,index)=>console.log(number/index+2))
+
+//- Crea una función que reciba un array de palabras y devuelva un array con las mismas palabras en mayúsculas.
+
+const arrayWords=['hola','adios','perro','gato']
+
+arrayWords.map((word)=>console.log(arrayWords.toUpperCase()))
