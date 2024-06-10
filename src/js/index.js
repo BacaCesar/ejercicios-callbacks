e356cb6001e877e672539467eea0284eabf07a69
BacaCesar <cesarbaca.fotografia@gmail.com>
2024-06-10 16:29:50 +0200

    update

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,8 @@
 // El styles lo importamos aquí para que se encargue Vite de compilar todo
 import '../scss/styles.scss';
+
+
+//- Crea una función que reciba un array de números y muestre por consola cada número multiplicado por su índice en el array
+
+const numbers=[1,2,3,4,5]
+console.log(numbers)
